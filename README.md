a7bd7db779b286a7af782ed7d0da7d8f5ff23055
Chris Canipe <chris.canipe@thomsonreuters.com>
2019-10-23 10:01:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+## Data scripting template
+
+This is a Node.js script that I often use to clean and shape large datasets. It includes an async parsing function that can pull in multiple JSON, CSV, TSV and Excel files. I typically load a few files, then pass them to a `setData()` function where I loop through the datasets to combine, clean and perform operations. Once you have a clean object array, you can save your data in a new file as either JSON or CSV.
+
+---
+
+### How to use this template.
+
+1) Clone this repository to your working directory.
+
+2) Using Terminal, `cd` into the directory.
+
+3) run `npm install`.
+
+4) Load your datasets via relative path, then output them locally via the `writeJSON()` or `writeCSV()` functions.
+
+5) To run the script in the Terminal, type: `node script.js`.
+
+6) Millertime.
+
+---
+
+### Named Functions and what they do
+
+#### `init()`
+Contains an `async` function that lets you load an array of data requests. Simply add as many files as you wish to the array, then catch them in the `results` array in the callback. The called data will appear in the `results` array in the order in which the cals they were made.
+
+## CSV, TSV, and JSON files
+
+The `async` function uses the `d3-fetch` parsing method, which can parse CSV, TSV, and JSON among other formats. Simple parse the requested output before passing it to the callback.
+
+## Excel files
+
+The Excel parser returns an array of objects, one for each worksheet in the file:
+
+```
+[{
+    name : [[WORKSHEET NAME]]
+    data : [[THIS DATA IS A RAW ARRAY OF ARRAYS]]
+}]
+```
+
+You'll need to run that `data` array through the `toObjectArray()` function before handing it off to your `setData()` function.
+
+
+#### `init()`
+
+
+
+
+
+To build a chart in D3, you really only need to worry about the `/src` directory. 
+
+This template takes an object-oriented approach to making charts in D3. In a nutshell, it's set up so you can write your chart script as a library or "module" that's then called from your main script. To learn more about this approach and why it works so well with D3, check out [this explainer](http://elliotbentley.com/2017/08/09/a-better-way-to-structure-d3-code-es6-version.html).
+
+`src/js/app.js` is the main JavaScipt file. Think of this as your index page for JavaScript. Only the top level stuff goes here.
+`src/js/chart-template.js` is 
+
+---
+
+### Available Gulp commands
+
+#### `gulp serve`
+Sets up a local server run out of `.tmp`. Watches your Sass, Handlebars and Javascript files and updates live in the browser.
+
+The `gulp serve` task will default to `port 3000`, which you can view in browser as `http://localhost:3000/`.
+
+Use the `-p` flag if you want to use a different port (for example, if you're already serving a proejct in port 3000, type `gulp serve -p 3002` to serve in port 3002.)
+
+#### `gulp build`
+Compiles your code into a single publishable directory called `/dist`
+
---
+++ script.js
@@ -0,0 +1,131 @@
+const xlsx = require('node-xlsx');
+const fs = require("fs");
+const _ = require('lodash');
+const request = require("request");
+const async = require("async");
+const json2csv = require('json2csv');
+const d3 = require('d3');
+
+function init() {
+
+    /* ===+===+===+===+===+===+=== */
+    /* D A T A   C A L L S 📞 */
+    /* ===+===+===+===+===+===+=== */
+
+    //SET UP YOUR DATA CALLS HERE.
+    //THIS HAPPENS IN AN ASYNC ARRAY SO TAHT WE CAN STACK MULTIPLE CALLS IF WE WANT TO
+    //JUST NEST THEM IN THE ARRAY
+    async.series(
+        [
+
+            function(callback) { //CSV EXAMPLE
+                fs.readFile('hca_enrollment_dec17.csv', 'utf8', function(err, d) {
+                    let data = d3.csvParse(d); //USE THE D3 CSV PARSER TO GET OBJECT ARRAY
+                    callback(null, data);
+                });
+            },
+            function(callback) { //EXCEL EXAMPLE
+                var data = xlsx.parse('nst-est2017-01.xlsx');
+                callback(null, data);
+            }
+
+        ],
+        function(err, results) {
+
+            // `results` is an array responses from the above requests
+            // The Excel parser returns as an object that looks like this:
+
+            // [{
+            //     name : [[WORKSHEET NAME]]
+            //     data : [[THIS DATA IS A RAW ARRAY OF ARRAYS]]
+            // }]
+
+            let hca = results[0];
+            let pop = toObjectArray(results[1][1].data); //USE THE toObjectArray() FUNCTION TO CONVERT THE EXCEL DATA TO AN OBJECT ARRAY
+
+            setData.init(hca, pop);
+
+        }
+    )
+
+
+}
+
+
+var setData = {
+    init: function(data1, data2) {
+
+        let theData = [];
+        let theLookup = {};
+
+        //The goal is usally to get your dataset organized as a clean array of objects.
+        //I often append data to a dictionary-type object called `theLookup`, then spin that out into an array.
+        //That array is then passed to either `writeJSON()` or `writeCSV`
+
+        writeJSON(theData);
+
+    }
+
+}
+
+
+function writeJSON(data) {
+
+    //Stringify the data
+    var theJson = JSON.stringify(data);
+
+    //...and write it to an output.json file. (or whatever you want to call it)
+    fs.writeJSON("data.json", theJson, function(err) {
+        if (err) return console.log(err);
+        console.log('Data Success.');
+    });
+
+
+}
+
+
+function writeCsv(data) {
+
+    let fields = Object.keys(data[0]); //Column headers (must exist in each object in your array)
+    let json2csvParser = new json2csv({ fields });
+    let csv = json2csvParser.parse(data);
+
+    //...and write it to an output.json file. (or whatever you want to call it)
+    fs.writeFile("data.csv", csv, err=> {
+        if (err) return console.log(err);
+        console.log('Data Success.');
+    });
+
+}
+
+
+
+
+/* ===+===+===+===+===+===+=== */
+/* U T I L I T I E S */
+/* ===+===+===+===+===+===+=== */
+
+
+//Converts that crazy Excel data format into an object array.
+function toObjectArray(origArray) {
+
+    var newArray = [];
+    for (var index = 1; index < origArray.length; index++) {
+        newArray.push(_.zipObject(origArray[0], origArray[index]));
+    }
+
+    return newArray;
+
+}
+
+//Add leading zeros to integers and return them as strings.
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+//Round a number to a specified decimal place
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
